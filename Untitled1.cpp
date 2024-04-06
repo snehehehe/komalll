7cfd6bd9b9c1e8c8f4f625020bd6c954084134fe
Sneha Patil <snehapatil130506@gmail.com>
2024-04-06 18:28:39 +0530

    commiting file in design branch

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 using namepsace std;
-void add(int x, int y)
+void addition(int x, int y)
 {
 	cout<<x+y;
+}
+void mul(int x, int y)
+{
+	cout<<x*y;
 }
 int main()
 {
 	int a=6, b=6;
 	add(a,b);
+	mul(a,b);
 	return 0;
 }
